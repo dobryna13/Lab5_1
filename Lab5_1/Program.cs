a368997938e500010e920aa91a2590a50c658687
dobryna13 <mdobrinchuk@gmail.com>
2023-04-09 22:27:06 +0300

    Добавьте файлы проекта.

---
+++ Lab5_1/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+class Program
+{
+    static Queue<int> buffer = new Queue<int>();
+    static object bufferLock = new object();
+
+    static void Main(string[] args)
+    {
+        Thread producerThread = new Thread(Producer);
+        Thread consumerThread = new Thread(Consumer);
+
+        producerThread.Start();
+        consumerThread.Start();
+
+        producerThread.Join();
+        consumerThread.Join();
+    }
+
+    static void Producer()
+    {
+        Random random = new Random();
+
+        while (true)
+        {
+            int number = random.Next(100);
+            lock (bufferLock)
+            {
+                buffer.Enqueue(number);
+                Console.WriteLine($"Producer produced: {number}");
+            }
+
+            Thread.Sleep(random.Next(1000));
+        }
+    }
+
+    static void Consumer()
+    {
+        while (true)
+        {
+            int number;
+            lock (bufferLock)
+            {
+                if (buffer.Count == 0)
+                {
+                    continue;
+                }
+
+                number = buffer.Dequeue();
+            }
+
+            Console.WriteLine($"Consumer consumed: {number}");
+            Thread.Sleep(500);
+        }
+    }
+}
+
